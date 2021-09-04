51739bb6a9fb3f00960f34d19ee0a037f11de251
Shubhamsharma585 <shubhamsharma585@gmail.com>
2021-09-04 18:10:16 +0530

    static tags builded

--- music.js
+++ music.js
@@ -8,21 +8,45 @@ const songs = document.getElementById("song");
 var canvas = document.getElementById("myCanvas")
 var ctx = canvas.getContext("2d");
 
-var width = 5;
+var width = 4;
 var X = 50;
-var max = 100;
+var max = 80;
 var min = -100;
 
-ctx.fillStyle = "#008080";
+ctx.fillStyle = "gray";
 
-for(var i=0; i<=200; i++)
+for(var i=0; i<=220; i++)
 {
     var h = Math.floor(Math.random()* (max-min) + min);
-    ctx.fillRect(X,90,width, h);
-    X = X + width+5;
+    ctx.fillRect(X,160,width, h);
+    X = X + width+2;
 }
 
+//ctx.beginPath();
+ctx.rect(200, 30, 3, 100);
+ctx.rect(150, 30, 100, 20);
 
+ctx.rect(400, 20, 3, 100);
+ctx.rect(350, 10, 100, 20);
+ctx.fillStyle = "lightgreen";
+ctx.fill();
+
+
+ctx.rect(1100, 20, 3, 130);
+ctx.rect(1050, 10, 100, 20);
+
+ctx.rect(1300, 20, 3, 130);
+ctx.rect(1250, 10, 100, 20);
+ctx.fill();
+
+
+ctx.rect(1180, 100, 3, 50);
+ctx.rect(1180, 100, 80, 20);
+ctx.fill();
+
+ctx.rect(1270, 60, 3, 90);
+ctx.rect(1120, 60, 150, 20);
+ctx.fill();
 
 
 

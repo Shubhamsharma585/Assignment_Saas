112d3f9c1361eff424e21dc98c728d0d4cc693ca
Shubhamsharma585 <shubhamsharma585@gmail.com>
2021-09-04 15:06:12 +0530

    music is added and starting to work on progress bar

--- music.js
+++ music.js
@@ -1,19 +1,24 @@
 const playBtn = document.getElementsByClassName("play")[0];
-const playIcon = document.getElementById("play_pause")
+const playIcon = document.getElementById("play_pause");
+const songs = document.getElementById("song");
+
+
 let isPlaying = false;
 
-
+console.log(songs)
 
 //To play music
 const playMusic = () => {
    isPlaying = true;
    playIcon.classList.replace("fa-play","fa-pause")
+   songs.play();
    //console.log("play")
 }
 
 //To pause music
 const pauseMusic = () => {
     isPlaying = false;
+    songs.pause();
     playIcon.classList.replace("fa-pause","fa-play")
     //console.log("pause")
 }

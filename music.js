76d88a480a78656f24262ee63c7dffa9a62ac4be
Shubhamsharma585 <shubhamsharma585@gmail.com>
2021-09-04 22:20:27 +0530

    trying to loop through colors

--- music.js
+++ music.js
@@ -5,47 +5,63 @@ const songs = document.getElementById("song");
 
 
 //var graphValues = [80,20,30,40,50];
-var canvas = document.getElementById("myCanvas")
+var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
 var width = 4;
-var X = 50;
+var X = 0;
 var max = 80;
 var min = -100;
 
-ctx.fillStyle = "gray";
 
-for(var i=0; i<=220; i++)
+
+
+for(var i=0; i<=250; i++)
 {
     var h = Math.floor(Math.random()* (max-min) + min);
-    ctx.fillRect(X,160,width, h);
+    ctx.fillStyle = "gray";
+    if(i<100)
+    {
+        ctx.fillStyle = "red";
+    }else{
+        ctx.fillStyle = "gray";
+    }
+    ctx.fillRect(X,210,width, h);
     X = X + width+2;
 }
 
-//ctx.beginPath();
-ctx.rect(200, 30, 3, 100);
-ctx.rect(150, 30, 100, 20);
 
-ctx.rect(400, 20, 3, 100);
-ctx.rect(350, 10, 100, 20);
+ctx.beginPath();
+ctx.rect(200, 70, 3, 100);
+ctx.rect(150, 70, 100, 20);
+
+ctx.rect(400, 40, 3, 140);
+ctx.rect(350, 30, 100, 20);
 ctx.fillStyle = "lightgreen";
 ctx.fill();
 
-
-ctx.rect(1100, 20, 3, 130);
+ctx.beginPath();
+ctx.rect(1100, 20, 3, 150);
 ctx.rect(1050, 10, 100, 20);
-
-ctx.rect(1300, 20, 3, 130);
-ctx.rect(1250, 10, 100, 20);
+ctx.fillStyle="green";
 ctx.fill();
 
-
-ctx.rect(1180, 100, 3, 50);
+ctx.beginPath();
+ctx.rect(1180, 100, 3, 70);
 ctx.rect(1180, 100, 80, 20);
+ctx.fillStyle="purple";
 ctx.fill();
 
-ctx.rect(1270, 60, 3, 90);
+ctx.beginPath();
+ctx.rect(1270, 60, 3, 110);
 ctx.rect(1120, 60, 150, 20);
+ctx.fillStyle="brown";
+ctx.fill();
+
+ctx.beginPath();
+ctx.rect(1300, 20, 3, 150);
+ctx.rect(1250, 10, 100, 20);
+ctx.fillStyle="green";
 ctx.fill();
 
 
@@ -54,6 +70,20 @@ ctx.fill();
 
 
 
+console.log(songs)
+
+
+
+
+
+
+songs.addEventListener('timeupdate', (event) => {
+    console.log(Math.floor(event.timeStamp/1000));
+  });
+
+  songs.addEventListener('loadmetadata', (event) => {
+    console.log(event);
+  });
 
 
 
@@ -62,8 +92,6 @@ ctx.fill();
 
 
 let isPlaying = false;
-
-console.log(songs)
 
 //To play music
 const playMusic = () => {

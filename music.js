b9ecd729fc83406c189a63e1e96c4ead0130b6f2
Shubhamsharma585 <shubhamsharma585@gmail.com>
2021-09-05 14:51:31 +0530

    Static tags improved

--- music.js
+++ music.js
@@ -2,12 +2,15 @@ const playBtn = document.getElementsByClassName("play")[0];
 const playIcon = document.getElementById("play_pause");
 const songs = document.getElementById("song");
 const seekBtn = document.getElementById("seek");
-
+let isPlaying = false;
 
 
 //var graphValues = [80,20,30,40,50];
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
+
+var canvas2 = document.getElementById("myCanvas2");
+var ctx2 = canvas2.getContext("2d");
 
 var width = 4;
 var X = 0;
@@ -15,20 +18,16 @@ var max = 80;
 var min = -100;
 
 
-
-
+ctx.beginPath();
 for(var i=0; i<=250; i++)
 {
     var h = Math.floor(Math.random()* (max-min) + min);
+
     ctx.fillStyle = "gray";
-    if(i<100)
-    {
-        ctx.fillStyle = "red";
-    }else{
-        ctx.fillStyle = "gray";
-    }
+
     ctx.fillRect(X,210,width, h);
-    X = X + width+2;
+    X = X + width+2; //Every time bar will move left by 6px;
+    //maximum width is 1400 so bars will cover width of (w<=1400)
 }
 
 
@@ -36,15 +35,16 @@ ctx.beginPath();
 ctx.rect(200, 70, 3, 100);
 ctx.rect(150, 70, 100, 20);
 
+
 ctx.rect(400, 40, 3, 140);
 ctx.rect(350, 30, 100, 20);
+
 ctx.fillStyle = "lightgreen";
 ctx.fill();
 
 ctx.beginPath();
 ctx.rect(1100, 20, 3, 150);
-ctx.rect(1050, 10, 100, 20);
-ctx.fillStyle="green";
+ctx.fillStyle="lightgreen";
 ctx.fill();
 
 ctx.beginPath();
@@ -54,24 +54,80 @@ ctx.fillStyle="purple";
 ctx.fill();
 
 ctx.beginPath();
+ctx.fillStyle="white";
+ctx.font = "10pt sans-serif";
+ctx.fillText("Introduction", 162, 85);
+
+
+ctx.beginPath();
 ctx.rect(1270, 60, 3, 110);
-ctx.rect(1120, 60, 150, 20);
+ctx.rect(1105, 60, 175, 20);
 ctx.fillStyle="brown";
 ctx.fill();
 
 ctx.beginPath();
 ctx.rect(1300, 20, 3, 150);
-ctx.rect(1250, 10, 100, 20);
+ctx.rect(1090, 10, 220, 20);
 ctx.fillStyle="green";
 ctx.fill();
 
+ctx.beginPath();
+ctx.arc(201, 178, 8, 0, 2 * Math.PI);
+ctx.fillStyle="lightgreen";
+ctx.fill();
+
+ctx.beginPath();
+ctx.arc(401, 178, 8, 0, 2 * Math.PI);
+ctx.fillStyle="lightgreen";
+ctx.fill();
+
+ctx.beginPath();
+ctx.arc(1101, 178, 8, 0, 2 * Math.PI);
+ctx.fillStyle="lightgreen";
+ctx.fill();
+
+ctx.beginPath();
+ctx.arc(1181, 178, 8, 0, 2 * Math.PI);
+ctx.fillStyle="purple";
+ctx.fill();
+
+ctx.beginPath();
+ctx.arc(1272, 178, 8, 0, 2 * Math.PI);
+ctx.fillStyle="brown";
+ctx.fill();
+
+ctx.beginPath();
+ctx.arc(1301, 178, 8, 0, 2 * Math.PI);
+ctx.fillStyle="green";
+ctx.fill();
+
+
+ctx.beginPath();
+ctx.fillStyle="white";
+ctx.font = "10pt sans-serif";
+ctx.fillText("One_six", 370, 45);
+
+ctx.beginPath();
+ctx.fillStyle="white";
+ctx.font = "10pt sans-serif";
+ctx.fillText("Rapport Building - Empathy", 1112, 75);
+
+ctx.beginPath();
+ctx.fillStyle="white";
+ctx.font = "10pt sans-serif";
+ctx.fillText("Rapport Building - Energy", 1100, 25);
+
+ctx.beginPath();
+ctx.fillStyle="white";
+ctx.font = "10pt sans-serif";
+ctx.fillText("Profile", 1200, 115);
 
 
 
 console.log(songs.duration)
 
 
-
+let Position = 0;
 
 
 
@@ -79,11 +135,25 @@ console.log(songs.duration)
 
 
 let duration = songs.duration;
-
+var X1 = 0;
 songs.addEventListener('timeupdate', (event) => {
+    //console.log(event.srcElement.currentTime)
+    current_time = event.srcElement.currentTime;
 
-    console.log(event.srcElement.currentTime)
+    Position = Math.floor((current_time*100)/duration);
+    //console.log(current_time, duration, Position)
+    console.log(Position)
 
+    if(Position == 100)
+    {
+        playIcon.classList.replace("fa-pause","fa-play");   
+    }
+
+    var h1 = Math.floor(Math.random()* (max-min) + min);
+    ctx2.fillStyle = "black";
+    ctx2.fillRect(X1,210,width, h1);
+    X1 = X1 + width+2;
+        
   });
 
  
@@ -92,7 +162,7 @@ songs.addEventListener('timeupdate', (event) => {
 
 
 
-let isPlaying = false;
+
 
 
 //To play music
@@ -123,7 +193,15 @@ playBtn.addEventListener("click", () => {
 
 
 seekBtn.addEventListener("click", () => {
-     seekMusic(68)
+     seekMusic(50)
 });
+
+canvas.addEventListener("mousedown", (event) => {
+    var total_length = 1400;
+    var local_position = event.clientX-18;
+    var covered_inper =  Math.floor(((local_position*100)/total_length));
+    console.log(local_position, total_length, covered_inper)
+    seekMusic(covered_inper)
+})
 
 
